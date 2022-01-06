dd3347431373cf174b3980a06aff2ee1d15c7c0a
Sarah Bunker <chemencoder@gmail.com>
2022-01-06 16:51:53 -0700

    cleaning up extra spaces.

--- book_viewer.rb
+++ book_viewer.rb
@@ -9,20 +9,20 @@ end
 
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  
+
   erb :home
 end
 
 get "/chapters/:number" do
   number = params[:number].to_i
-  
+
   redirect "/" unless(1..@contents.size).cover? number
-  
+
   @chapter = File.read("data/chp#{number}.txt")
-  
+
   chapter_name = @contents[number - 1]
   @title = "Chapter #{number}: #{chapter_name}"
-  
+
   erb :chapter
 end
 
@@ -38,12 +38,12 @@ helpers do
     end
     result.join
   end
-  
+
   def bold_search(text)
     text.gsub(@term, "<strong>#{@term}</strong>")
   end
 end
-  
+
 def each_chapter
   @contents.each_with_index do |name, index|
     number = index + 1
@@ -54,18 +54,17 @@ end
 
 def chapters_matching(query)
   results = []
-  
+
   return results if !query || query.empty?
-  
+
   each_chapter  do |name, number, text| 
     text.split("\n\n").each_with_index do |paragraph, id_num|
       results << {name: name, number: number, id_num: id_num, paragraph: paragraph} if paragraph.include?(query)
     end
   end
-  
+
   results
 end
-
 
 get "/search" do
   @term = params[:query]
